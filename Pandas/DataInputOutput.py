13394ef1abdc05e7d3f5544a13362c4ea8c9f877
Eugene Shrm <shramkoevhenii.com>
2024-11-28 21:55:59 +0200

    Add inputOutput folder and add CSV/ xlsx files

---
+++ Pandas/DataInputOutput.py
@@ -0,0 +1,45 @@
+import numpy as np
+import pandas as pd
+
+#read file CSV, JSON, HTML, etc
+read_csv = pd.read_csv("example")
+print(read_csv)
+
+#create file 
+My_output = read_csv.to_csv("My_output", index=False) # False is needed to skip the index, as it is already exist
+print(My_output)
+
+#read excel
+read_excel = pd.read_excel("Excel_Sample.xlsx", sheet_name='Sheet1') #sheet_name need to choose sheet/tab
+print(read_excel)
+
+#create excel
+My_excel = read_excel.to_excel("My_Excel_Sample.xlsx", sheet_name='Sheet2', index=False)
+print(My_excel)
+
+#read HTML, using for webscraping
+read_html = pd.read_html('http://www.fdic.gov/bank/individual/failed/banklist.html')
+type(read_html) #output: list, and each item is df
+print(read_html)
+print(read_html[0].head()) #choose first item of html 
+
+#read SQL
+from sqlalchemy import create_engine
+
+engine = create_engine('sqlite:///:memory:') # Create an in-memory SQLite database
+
+# Sample DataFrame to be written to SQL
+df = pd.DataFrame({
+    'column1': [1, 2, 3],
+    'column2': ['A', 'B', 'C']
+})
+
+# Write the DataFrame to the SQL database
+read_csv.to_sql('data', engine, index=False, if_exists='replace')
+
+# Read the data back from the SQL database
+sql_df = pd.read_sql('SELECT * FROM data', con=engine)
+
+# Print the result
+print(sql_df)
+
