b75e8bb8bd06f04e503af89aa2aeef0c38433042
Eugene Shrm <shramkoevhenii.com>
2024-12-30 16:16:14 +0200

    Categorical and Distribution plots

---
+++ Seaborn/CategoricalPlots.py
@@ -0,0 +1,39 @@
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+tips = sns.load_dataset("tips") #seaborn buildin dataset 
+tips = tips.head()
+print(tips)
+
+#cat 
+# plot is the most general form of a categorical plot. It can take in a kind parameter to adjust the plot type
+cat_plot = sns.catplot(x='sex',y='total_bill',data=tips,kind='bar') #can make bar, violing, etc
+plt.show()
+
+#bar plot - numerical vs categorical data 
+bar_plot = sns.barplot(x="sex", y= "total_bill", data=tips, palette="coolwarm")
+plt.show()
+
+#count plot
+count_plot = sns.countplot(x="sex", data = tips) #the same and pd.count
+"""sex_count = tips["sex"].count()
+print(sex_count)"""
+plt.show()
+
+#box plot 
+box_plot= sns.boxplot(x="day", y="total_bill", data=tips, hue = "smoker")
+plt.show()
+
+#violing plot
+violing_plot = sns.violinplot(x="day", y="total_bill", data=tips, hue = "sex", split=True)
+plt.show()
+
+#strip plot
+strip_plot = sns.stripplot(x="day", y="total_bill", data=tips,jitter=True) #jitter adding mess when there are a lot of dots 
+plt.show()
+
+#swarm plot - combination of strip plot and violing plot 
+swarm_plot = sns.swarmplot(x="day", y="total_bill",hue='sex',data=tips, palette="Set1", split=True)
+plt.show()
--- Seaborn/DistributionPlots.py
+++ Seaborn/DistributionPlots.py
@@ -1,7 +1,26 @@
 import numpy as np
 import seaborn as sns
+import matplotlib.pyplot as plt
 
 tips = sns.load_dataset("tips") #seaborn buildin dataset 
 print(tips)
-print(sns.displot(tips["total_bill"])) #use displot type
 
+#displot
+total_bill = sns.displot(tips["total_bill"], kde=True, bins=20) # add kde and bins
+# Adjust the x and y axes limits
+total_bill.ax.set_xlim(0, 50)  # Set x-axis range
+total_bill.ax.set_ylim(0, 100) 
+plt.show()
+
+#joinplot
+tips = sns.load_dataset("tips")
+joint_plot = sns.jointplot(x="total_bill", y="tip", data=tips, kind = "hex") # kind = hex/reg/kde
+plt.show()
+
+#pair plot
+pair_plot = sns.pairplot(tips, hue = "sex", palette="coolwarm")
+plt.show()
+
+#rug plot
+rug_plot = sns.rugplot(tips["total_bill"])
+plt.show()
