8dc386a285a0ac8d95702400790fd3d7adc3b660
Eugene Shrm <shramkoevhenii.com>
2025-01-11 09:33:00 +0200

    Add Cufflinks file

--- PythonForAI/Pandas/DataVisualizationExercise.py
+++ PythonForAI/Pandas/DataVisualizationExercise.py
@@ -5,9 +5,8 @@ df3 = pd.read_csv('df3')
 print(df3)
 
 #ex1
-# Create a scatter plot from the DataFrame `df3`
+# Create a scatter plot from the DataFrame `df3`, play around with the color and size of the points
 df3.plot.scatter(
-    x='a',          # Column 'a' is used for the x-axis
     y='b',          # Column 'b' is used for the y-axis
     c='red',        # Points are colored red
     s=50,           # Size of each scatter point is set to 50
@@ -17,11 +16,11 @@ df3.plot.scatter(
 # Display the plot
 plt.show()
 
-#ex2
+#ex2 Create a histogram of the 'a' column
 plt.style.use('ggplot')
 df3['a'].plot.hist(alpha=0.5,bins=25)
 plt.show()
 
-#ex3
-df3.plot.box(x= "a", y = "b")
+#ex3 Create a boxplot comparing the a and b columns
+df3[['a','b']].plot.box()
 plt.show()
